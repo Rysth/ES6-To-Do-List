83ea5b709d7aaae2d2407dae0681f78d1234c54e
kambereBr <brunokambere@gmail.com>
2023-07-20 18:33:01 +0200

    Add test function for the removeTask operation

--- src/modules/functionality.test.js
+++ src/modules/functionality.test.js
@@ -12,3 +12,15 @@ describe('Add Functionality', () => {
 });
 
 /* Bruno: removeTask() */
+describe('Remove Functionality', () => {
+  test('Should remove a new task within the collection', () => {
+    const collection = [
+      { index: 1, description: 'Hello World 1', complete: false },
+      { index: 2, description: 'Hello World 2', complete: true }
+    ];
+    removeTask(0, collection);
+    expect(collection).toEqual([
+      { index: 1, description: 'Hello World 2', complete: true },
+    ]);
+  });
+});
